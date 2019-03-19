646122ae8fd99a9a0af733e082ed538c3f1b6e9a
shoma <shoma@catbot.net>
2019-03-19 14:03:48 +0900

    Remove unused test code from main

--- main.go
+++ main.go
@@ -1,34 +1,10 @@
 package main
 
 import (
-	"context"
 	"github.com/shoma/rcid/rcid"
-	"github.com/shoma/rcid/runner"
-	"log"
-	"time"
+	"os"
 )
 
 func main() {
-	rcid.Main()
-	//testRunner()
-}
-
-func testRunner() {
-	args := []string{"images"}
-	cmd := runner.NewCommand(
-		"/usr/local/bin/docker",
-		args,
-		"",
-		nil,
-		60*time.Second,
-	)
-	cmd.SetStdoutFn(log.Print)
-	cmd.SetStderrFn(log.Print)
-	cmd.SetCtx(context.Background())
-	err := cmd.Run()
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
+	os.Exit(rcid.Main())
 }
